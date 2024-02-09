c62233659507e5a84951aef9fcc6a837d484f678
Sinay Dori <dori.sinay@post.runi.ac.il>
2024-02-09 12:10:23 +0200

    Changed Tokenizer to JackTokenizer and created declarations for all functions of JackTokenizer and CompilationEngine

---
+++ 10/CompilationEngine.py
@@ -0,0 +1,65 @@
+class CompileEngine:
+
+    def __init__(self, File):
+        # TODO: implement
+        pass
+
+    def compileClass(self):
+        # TODO: implement
+        pass
+
+    def compileClassVarDec(self):
+        # TODO: implement
+        pass
+
+    def compileSubroutine(self):
+        # TODO: implement
+        pass
+
+    def compileParameterList(self):
+        # TODO: implement
+        pass
+
+    def compileSubroutineBody(self):
+        # TODO: implement
+        pass
+
+    def compileVarDec(self):
+        # TODO: implement
+        pass
+
+    def compileStatements(self):
+        # TODO: implement
+        pass
+
+    def compileLet(self):
+        # TODO: implement
+        pass
+
+    def compileIf(self):
+        # TODO: implement
+        pass
+
+    def compileWhile(self):
+        # TODO: implement
+        pass
+
+    def compileDo(self):
+        # TODO: implement
+        pass
+
+    def compileReturn(self):
+        # TODO: implement
+        pass
+
+    def compileExpression(self):
+        # TODO: implement
+        pass
+
+    def compileTerm(self):
+        # TODO: implement
+        pass
+
+    def compileExpressionList(self):
+        # TODO: implement
+        pass
---
+++ 10/JackTokenizer.py
@@ -0,0 +1,37 @@
+class JackTokenizer:
+
+    def __init__(self, File):
+        # TODO: implement
+        pass
+
+    def hasMoreTokens(self):
+        # TODO: implement
+        pass
+
+    def advance(self):
+        # TODO: implement
+        pass
+
+    def tokenType(self):
+        # TODO: implement
+        pass
+
+    def keyWord(self):
+        # TODO: implement
+        pass
+
+    def symbol(self):
+        # TODO: implement
+        pass
+
+    def identifier(self):
+        # TODO: implement
+        pass
+
+    def intVal(self):
+        # TODO: implement
+        pass
+
+    def stringVal(self):
+        # TODO: implement
+        pass
